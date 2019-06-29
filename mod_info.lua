d5c48dee4524d4cf8f082f96a843dad02391eb26
Matthew Clarke <soniduino@gmail.com>
2019-06-29 20:07:38 +0100

    Initial Commit

---
+++ mod_info.lua
@@ -0,0 +1,14 @@
+name = "HiveNoNukeDef"
+uid = "24a0c2f6-59b6-489e-bfff-6bba00009999"
+version = 0
+copyright = "PegasusScourge"
+description = "Prevents hives from auto assisting nuke defence in range"
+author = "PegasusScourge"
+url = "https://github.com/PegasusScourge/HiveNoNukeDef"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+conflicts = {
+	--"24a0c2f6-59b6-489e-a899-6bba42999999", -- version 1.0 of the mod
+}
